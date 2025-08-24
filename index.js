7156426ae970318e1245c0008c23a95ba641495e
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-08-24 09:29:14 +0500

    33333

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ const PORT = process.env.PORT || 8040;
 const notfound = require("./middleware/notfound.middleware");
 const router = require("./routes/router");
 const authMiddleware = require("./middleware/AuthMiddleware");
-
 const { createServer } = require("node:http");
-
 const soket = require("./socket");
-
 const AttendanceController = require("./controller/attendanceController");
 
 const app = express();
